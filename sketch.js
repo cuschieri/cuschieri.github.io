cba4d9d46a18a2987f8fc8bdaf7154690f59d2ee
cuschieri <34061291+cuschieri@users.noreply.github.com>
2024-09-12 14:45:00 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -5,15 +5,25 @@ function setup() {
   current_colour = color(200,0,0);
   background(0);
   noStroke();
+  fill(255,255,255);
+  textFont('Courier New');
+  textSize(12);
+  text('Left button to draw.\nRight click to clear.',windowWidth/32,windowHeight/16);
 }
 
 function draw() {
+ 
   fill(current_colour);
   let mouseSpeed = abs(sqrt((mouseX - pmouseX)*(mouseX - pmouseX)+(mouseY - pmouseY)*(mouseY - pmouseY)));
   if (mouseIsPressed) {
     ellipse(mouseX,mouseY,abs(50-5*mouseSpeed));
     if (mouseButton === RIGHT) {
       background(0);
+    noStroke();
+    fill(255,255,255);
+    textFont('Courier New');
+    textSize(12);
+    text('Left button to draw.\nRight click to clear.',windowWidth/32,windowHeight/16);
     }
   }
 }
